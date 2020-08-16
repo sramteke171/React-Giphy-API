516c0322b829b23a1665efc3fa55ac76afc4b9b2
Sachin Ramteke <sachin06jd@gmail.com>
2020-08-16 16:58:22 -0500

    Search working

--- giphy-app/src/App.js
+++ giphy-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
 import GiphyList from "./GiphyList";
+import Giphy from "./Giphy";
 
 class App extends Component {
   constructor() {
@@ -9,14 +10,15 @@ class App extends Component {
 
     this.state = {
       giphies: [],
+      searchInput: "",
     };
   }
 
   componentDidMount = () => {
     axios
       .get(
-        "https://api.giphy.com/v1/gifs/search?api_key=Hrvht7esx5VuAneCKQ6apeDDwy5kD9ZT&q=pizza/",
-        // "https://api.giphy.com/v1/gifs/search?api_key=Hrvht7esx5VuAneCKQ6apeDDwy5kD9ZT&q=${userInput}",
+        // "https://api.giphy.com/v1/gifs/search?api_key=Hrvht7esx5VuAneCKQ6apeDDwy5kD9ZT&q=pizza/",
+        "https://api.giphy.com/v1/gifs/search?api_key=Hrvht7esx5VuAneCKQ6apeDDwy5kD9ZT&q=${userInput}",
         {
           headers: {
             Accept: "application/json",
@@ -29,14 +31,72 @@ class App extends Component {
         });
       });
   };
-  randomGiphy = () => {
-    alert("Random giphy ");
+
+  randomGiphy = (event) => {
+    // return <GiphyList giphies={this.state.giphies} />;
+    // <img src={props.giphy.images.original.url}></img>;
+
+    axios
+      .get(
+        "https://api.giphy.com/v1/gifs/search?api_key=Hrvht7esx5VuAneCKQ6apeDDwy5kD9ZT&q=pizza/"
+      )
+      .then((response) => {
+        this.setState({
+          giphies: response.data.data,
+        });
+      });
   };
-  trendingGiphy = () => {
-    alert("Trending Giphy");
+
+  trendingGiphy = (event) => {
+    axios
+      .get(
+        "https://api.giphy.com/v1/gifs/search?api_key=Hrvht7esx5VuAneCKQ6apeDDwy5kD9ZT&q=sport"
+      )
+      .then((response) => {
+        this.setState({
+          giphies: response.data.data,
+
+          // return(" <div> <img src={props.giphy.images.original.url}></img> </div>")
+        });
+      });
   };
-  getGif = () => {
-    alert("Get Gif");
+
+  handleChange = (event) => {
+    this.setState({
+      searchInput: event.target.value,
+    });
+    console.log("event.target.value", event.target.value);
+  };
+
+  //search
+  handleSearch = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      searchInput: this.state.searchInput,
+    });
+    console.log("handleSearch- this.state.searchInput", this.state.searchInput);
+
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=Hrvht7esx5VuAneCKQ6apeDDwy5kD9ZT&q=${this.state.searchInput}/`;
+    console.log(url);
+
+    axios
+      .get(
+        // "https://api.giphy.com/v1/gifs/search?api_key=Hrvht7esx5VuAneCKQ6apeDDwy5kD9ZT&q=pizza/"
+        // https://api.giphy.com/v1/gifs/search?api_key=Hrvht7esx5VuAneCKQ6apeDDwy5kD9ZT&q=pizza/
+
+        `https://api.giphy.com/v1/gifs/search?api_key=Hrvht7esx5VuAneCKQ6apeDDwy5kD9ZT&q=${this.state.searchInput}/`
+      )
+      .then((response) => {
+        this.setState({
+          giphies: response.data.data,
+        });
+
+        console.log(this.state.giphies);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
   };
 
   render() {
@@ -56,8 +116,18 @@ class App extends Component {
             value="Get trending giphy!"
             onClick={this.trendingGiphy}
           ></input>
-          <input name="search" type="text" value={this.state.search}></input>
-          <input type="submit" value="Get Gif!" onClick={this.getGif}></input>
+          <input
+            name="searchInput"
+            type="text"
+            value={this.state.searchInput}
+            placeholder="search Input"
+            onChange={this.handleChange}
+          ></input>
+          <input
+            type="submit"
+            value="Get Gif!"
+            onClick={this.handleSearch}
+          ></input>
         </div>
         <GiphyList giphies={this.state.giphies} />
       </div>
